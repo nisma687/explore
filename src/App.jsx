bccc353e2de689544acdd43a40d0cf6281468d80
nisma687 <20-41982-1@student.aiub.edu>
2024-01-02 13:27:23 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    
+      <h1 className='text-2xl
+      font-semibold
+       text-center mt-5'>Explore Page</h1>
+      
+     
+    </>
+  )
+}
+
+export default App
